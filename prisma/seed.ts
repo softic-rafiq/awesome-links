2b51d66d44166a32382fd4b21c55076c47785868
Md Rafiqul Islam <119554804+softic-rafiq@users.noreply.github.com>
2023-08-27 11:46:43 +0600

    Prisma setup

---
+++ prisma/seed.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+import { links } from "../data/links";
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      email: `rafiqul@gmail.com`,
+      role: "ADMIN",
+    },
+  });
+
+  await prisma.link.createMany({
+    data: links,
+  });
+}
+
+main()
+  .catch((e) => {
+    console.log(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
